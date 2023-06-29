107d3202217bb13a52b1a983054c77569e94a69b
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-06-29 17:09:29 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import { burger } from './modules/_burger.js'
+import { sendForm } from "./modules/_form.js"
+import { hello } from './modules/_hello.js'
+import { initSlider } from "./modules/_initSlider.js"
+
+hello()
+burger()
+initSlider()
+sendForm()
