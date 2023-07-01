a1b3335bb93aa1db9959efbc95a51f559b287e79
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-07-02 02:09:23 +0300

    feat: подготовил к деплою и исправил некоторые баги

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
 import { burger } from './modules/_burger.js'
-import { sendForm } from "./modules/_form.js"
 import { hello } from './modules/_hello.js'
-import { initSlider } from "./modules/_initSlider.js"
 
 hello()
 burger()
-initSlider()
-sendForm()
